b6ea36950407722bb2affe8377f4e3e6842e62df
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-03-18 10:41:56 -0300

    Backend project created

---
+++ backend/src/test/java/com/leoabrantes/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoabrantes.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
