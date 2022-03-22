3f0b4ca5c6448b865163e17e689c0db1c0bdfcaf
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-03-22 15:21:40 -0300

    h2 Database part.01

---
+++ backend/src/main/java/com/leoabrantes/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.leoabrantes.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leoabrantes.dscatalog.entitties.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+
+}
