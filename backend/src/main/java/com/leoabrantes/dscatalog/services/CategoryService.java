0e5314baed8b228977e9493dfaf2744116163020
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-03-30 16:34:07 -0300

    DTO

--- backend/src/main/java/com/leoabrantes/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/leoabrantes/dscatalog/services/CategoryService.java
@@ -1,23 +1,30 @@
 package com.leoabrantes.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.leoabrantes.dscatalog.dto.CategoryDTO;
 import com.leoabrantes.dscatalog.entitties.Category;
 import com.leoabrantes.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
-	}
 
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		return listDTO;
+	}
 }
